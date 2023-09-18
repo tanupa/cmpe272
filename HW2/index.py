7333991a649a2bb422fffa5c084102600dc33f0e
Miranda Livengood <miranda.livengood@sjsu.edu>
2023-09-18 06:44:38 +0000

    Call create_tweet.py function in index.py.

--- HW2/index.py
+++ HW2/index.py
@@ -1,5 +1,6 @@
 #Done by Ben Kwon - delete-tweet and Flask back-end
 from flask import Flask
+import create_tweet
 
 app = Flask(__name__)
 
@@ -13,25 +14,8 @@ from requests_oauthlib import OAuth1Session
 import json
 
 @app.route('/create-tweet/<text>', methods=["POST"])
-def create_tweet(text):
-
-  oauth = OAuth1Session(
-    client_key=consumer_key,
-    client_secret = consumer_secret,
-    resource_owner_key = access_token,
-    resource_owner_secret = access_token_secret,
-  )
-
-  data = {"text": text}
-
-  response = oauth.post("https://api.twitter.com/2/tweets", json=data)
-
-  resJson = response.json()
-
-  if "status" in resJson and resJson['status'] == 403:
-    return "", 403
-  if "data" in resJson:
-    return resJson, 202
+def create(text):
+    return create_tweet(text)
 
 @app.route('/delete-tweet/<id>', methods=["DELETE"])
 def delete_tweet(id):
