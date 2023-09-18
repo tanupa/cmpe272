1ebac7a48135ff792724243f6ee9f67e500d2c18
Ben Kwon <31276839+evolentDelight@users.noreply.github.com>
2023-09-17 22:39:40 -0700

    Back-end code utilizing Flask
    Endpoint:
    localhost:5000/create-tweet/<text>
    localhost:5000/delete-tweet/<id>

---
+++ HW2/index.py
@@ -0,0 +1,51 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+consumer_key=""
+consumer_secret = ""
+access_token =""
+access_token_secret = ""
+
+import requests
+from requests_oauthlib import OAuth1Session
+import json
+
+@app.route('/create-tweet/<text>')
+def create_tweet(text):
+
+  oauth = OAuth1Session(
+    client_key=consumer_key,
+    client_secret = consumer_secret,
+    resource_owner_key = access_token,
+    resource_owner_secret = access_token_secret,
+  )
+
+  data = {"text": text}
+
+  response = oauth.post("https://api.twitter.com/2/tweets", json=data)
+
+  resJson = response.json()
+
+  if "status" in resJson and resJson['status'] == 403:
+    return "", 403
+  if "data" in resJson:
+    return resJson, 202
+
+@app.route('/delete-tweet/<id>')
+def delete_tweet(id):
+  oauth = OAuth1Session(
+  client_key=consumer_key,
+  client_secret = consumer_secret,
+  resource_owner_key = access_token,
+  resource_owner_secret = access_token_secret,
+  )
+
+  response = oauth.delete(f"https://api.twitter.com/2/tweets/{id}")
+
+  resJson = response.json()
+
+  if "data" in resJson and resJson['data']['deleted'] == True:
+    return "", 204
+  else:
+    return resJson
