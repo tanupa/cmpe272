1b044d938d799c42321b7318810b23633d812567
Ben Kwon <31276839+evolentDelight@users.noreply.github.com>
2023-09-17 19:37:28 -0700

    Added delete_tweet.py

---
+++ HW2/delete_tweet.py
@@ -0,0 +1,18 @@
+consumer_key=""
+consumer_secret = ""
+access_token =""
+access_token_secret = ""
+
+import requests
+from requests_oauthlib import OAuth1Session
+import json
+
+def delete(id):
+  oauth = OAuth1Session(
+  client_key=consumer_key,
+  client_secret = consumer_secret,
+  resource_owner_key = access_token,
+  resource_owner_secret = access_token_secret,
+  )
+
+  response = oauth.delete(f"https://api.twitter.com/2/tweets/{id}")
